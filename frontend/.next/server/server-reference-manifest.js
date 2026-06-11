d7e4bb07e2c0514a12aaba40480a183b1daf97fa
Kena-AT <Kenaararso4@gmail.com>
2026-06-11 21:04:05 +0300

    fixed Sprint 1

---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vOzlu0yzuojYwE/wTQYpS7St/CmB4zbo/rgQPkCKxMQ=\"\n}"
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen bg-background-primary flex flex-col`}>
+      <body className={`${inter.className} min-h-screen bg-background-primary flex flex-col text-text-primary`}>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
